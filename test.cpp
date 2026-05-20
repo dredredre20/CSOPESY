9fc6b8812b6abbe229ddfaf3720995a3372fa876
Andre <andrechu17@gmail.com>
2026-05-20 14:39:53 +0800

    feat: initial file for group activity

---
+++ test.cpp
@@ -0,0 +1,70 @@
+#include <iostream>
+#include <string>
+
+class ASCIIart{
+    public:
+        void printASCIIart(){
+            // CSOPESY
+        }
+};
+
+class MainMenu{
+    public:
+        void welcomeMenu(){
+            ASCIIart ascii;
+            ascii.printASCIIart();
+            std::cout << "Hello, Welcome to CSOPESY commandline!";
+            std::cout << "\nType 'exit' to quit, 'clear' to clear screen";
+            std::cout << "\n\n** IMPORTANT: Type 'initialize' to load config and start system **\n";
+        } 
+};
+
+// Global function to print menu
+void printMenu(){
+    MainMenu menu;
+    menu.welcomeMenu();
+}
+
+class CommandManager{
+
+    public:
+        bool processCommand(){
+            bool flag = true;
+
+            std::string command;
+            std::cout <<"\nEnter a command: ";
+            std::getline(std::cin, command);
+
+            if (command == "initialize"){
+                std::cout << command << "command recognized. Doing something.";
+            } else if (command == "screen"){
+                std::cout << command << "command recognized. Doing something.";
+            } else if (command == "scheduler-start"){
+                std::cout << command << "command recognized. Doing something.";
+            } else if (command == "scheduler-stop"){
+                std::cout << command << "command recognized. Doing something.";
+            } else if (command == "report-util"){
+                std::cout << command << "command recognized. Doing something.";
+            } else if (command == "clear"){
+                std::cout << "\033[2J\033[1;1H";    
+                printMenu();
+            
+            } else if (command == "exit"){
+                flag = false;
+                exit(0);
+            } else {
+                std::cout << "Unknown command. Try again";
+            }
+
+            return flag;
+        }
+};
+
+int main(){
+    printMenu();
+    CommandManager cmdManager;
+
+    while (cmdManager.processCommand()){
+
+    }
+}
