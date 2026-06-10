6ce3518e9f9f1c94d2efff9f946eba37d1dae2aa
kharlene_monloy <kharlene_monloy@dlsu.edu.ph>
2026-06-10 16:15:34 +0800

    feat: create ImGui + OpenGL + GLFW setup

---
+++ main.cpp
@@ -0,0 +1,61 @@
+﻿#include "imgui.h"
+#include "imgui_impl_glfw.h"
+#include "imgui_impl_opengl3.h"
+#include <GLFW/glfw3.h>
+#include <cstdio>
+
+int main()
+{
+    glfwInit();
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 0);
+
+    GLFWwindow* window = glfwCreateWindow(960, 540, "CSOPESY Desktop OS Emulator", nullptr, nullptr);
+    if (!window)
+    {
+        printf("[ERROR] glfwCreateWindow() failed\n");
+        glfwTerminate();
+        return -1;
+    }
+    printf("[OK] GLFW window created\n");
+    glfwMakeContextCurrent(window);
+
+    //gladLoadGLLoader((GLADloadproc)glfwGetProcAddress);
+
+    IMGUI_CHECKVERSION();
+    ImGui::CreateContext();
+
+    ImGuiIO& io = ImGui::GetIO();
+    io.IniFilename = nullptr;
+
+    ImGui_ImplGlfw_InitForOpenGL(window, true);
+    ImGui_ImplOpenGL3_Init("#version 330");
+    printf("[OK] Dear ImGui initialized\n");
+
+    // Kernel initialization
+    // Start system services
+
+    printf("[OK] Entering main loop\n");
+    while (!glfwWindowShouldClose(window)) {
+        glfwPollEvents();
+
+        ImGui_ImplOpenGL3_NewFrame();
+        ImGui_ImplGlfw_NewFrame();
+        ImGui::NewFrame();
+
+        // Update application logic
+        // Render all UI components
+        ImGui::Render(); // finalize
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+        glfwSwapBuffers(window);
+    }
+
+    ImGui_ImplOpenGL3_Shutdown();
+    ImGui_ImplGlfw_Shutdown();
+    ImGui::DestroyContext();
+    glfwDestroyWindow(window);
+    glfwTerminate();
+
+    return 0;
+}
