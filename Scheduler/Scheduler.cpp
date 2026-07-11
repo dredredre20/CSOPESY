e3b4794c1b66f67ea44dba16a5504171d7d6b41a
angelorocha <163288359+AngeloHRocha@users.noreply.github.com>
2026-07-11 17:26:26 +0800

    chore: adjust log format

--- Scheduler/Scheduler.cpp
+++ Scheduler/Scheduler.cpp
@@ -161,8 +161,8 @@ void Scheduler::writeQuantumReport() {
 
     report << "Timestamp: " << getTimestamp() << "\n";
     report << "Number of processes in memory: " << memoryResidentProcesses.size() << "\n";
-    report << "\nTotal external fragmentation in KB: " << externalFragmentation << "\n";
-    report << "----end---- = " << totalMemory << "\n\n";
+    report << "Total external fragmentation in KB: " << externalFragmentation << "\n";
+    report << "\n----end---- = " << totalMemory << "\n\n";
 
     for (const auto& block : blocks) {
         const size_t upperLimit = block.start + block.size;
